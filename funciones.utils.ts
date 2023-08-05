0c9a99a0bf5daacc358dc47f36c067d3935c1d45
Juan Manuel Alvarez <30739456+2jmalvarez@users.noreply.github.com>
2023-08-05 18:16:42 -0300

    Create funciones.utils.ts

---
+++ funciones.utils.ts
@@ -0,0 +1,60 @@
+import fs from "fs";
+export function secondsToString(seconds: number) {
+  let hour = Math.floor(seconds / 3600) as any;
+  hour = hour < 10 ? "0" + hour : hour;
+  let minute = Math.floor((seconds / 60) % 60) as any;
+  minute = minute < 10 ? "0" + minute : minute;
+  let second = (seconds % 60) as any;
+  second = second < 10 ? "0" + second : second;
+  return hour + ":" + minute + ":" + second;
+}
+
+export function guardarLog(nombreArchivoLog: string, mensaje: string) {
+  if (!fs.existsSync("./logs")) fs.mkdirSync("./logs");
+  fs.writeFileSync(`./logs/${nombreArchivoLog}.log`, mensaje, { flag: "a+" });
+}
+
+export const querySql = (nombreArchivoSql: string) =>
+  fs
+    .readFileSync(`./src/sql/${nombreArchivoSql}.sql`, "utf-8")
+    .replace(/[\r\n]/g, " ");
+
+export const mapVariables = (result: any[]) =>
+  result?.map((d) =>
+    Object.fromEntries(
+      Object.entries(d).map((o) => [
+        o[0].toLowerCase().replace(/_./g, (x) => x[1].toUpperCase()),
+        o[1],
+      ])
+    )
+  );
+
+export const agrupadorGenerico = ({
+  data,
+  nombreIdentificador,
+  columnasParaAgrupar,
+  nombreVariableAgrupada,
+}: {
+  data: any[];
+  nombreIdentificador: string;
+  columnasParaAgrupar: string[];
+  nombreVariableAgrupada: string;
+}) =>
+  [...new Set(data.map((d) => d[nombreIdentificador]))]
+    .map((_) => ({
+      ...data.find((d) => d[nombreIdentificador] === _),
+      [nombreVariableAgrupada]: data
+        .filter(
+          (d) => d[nombreIdentificador] === _ && d[columnasParaAgrupar[0]]
+        )
+        .map((d) =>
+          Object.fromEntries(
+            Object.entries(d).filter((t) => columnasParaAgrupar.includes(t[0]))
+          )
+        ),
+    }))
+    .map((d) =>
+      Object.fromEntries(
+        Object.entries(d).filter((t) => !columnasParaAgrupar.includes(t[0]))
+      )
+    );
